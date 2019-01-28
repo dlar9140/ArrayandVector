7a824d9f1e122fcac6ba66b252a7381c320d69c8
David <davidlarson46@yahoo.com>
2019-01-28 09:53:45 -0700

    Creating a vector.

---
+++ ArrayandVector/Controller/Controller.cpp
@@ -0,0 +1,17 @@
+//
+//  Controller.cpp
+//  ArrayandVector
+//
+//  Created by Larson, David on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+#include <iostream>
+#include <vector>
+
+std::vector<int> v;
+
+v.push_back(2);
+v.push_back(3);
+v.push_back(1);
